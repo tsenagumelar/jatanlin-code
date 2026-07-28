e98902b60dcbd6c2e06242d2d9542d5798f72368
Taufan Sena Gumelar <tsena.gumelar@gmail.com>
2026-07-29 06:08:57 +0700

    fix config minio

--- jatanlin-revamp/services/backend/internal/api/app.go
+++ jatanlin-revamp/services/backend/internal/api/app.go
@@ -1,6 +1,9 @@
 package api
 
 import (
+	"os"
+	"strconv"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/logger"
@@ -9,7 +12,8 @@ import (
 
 func newFiberApp() *fiber.App {
 	app := fiber.New(fiber.Config{
-		AppName: "WIM Service API",
+		AppName:   "WIM Service API",
+		BodyLimit: bodyLimitBytes(),
 	})
 
 	app.Use(recover.New())
@@ -21,4 +25,17 @@ func newFiberApp() *fiber.App {
 	}))
 
 	return app
+}
+
+func bodyLimitBytes() int {
+	const defaultLimitMB = 50
+	value := os.Getenv("API_BODY_LIMIT_MB")
+	if value == "" {
+		return defaultLimitMB * 1024 * 1024
+	}
+	limitMB, err := strconv.Atoi(value)
+	if err != nil || limitMB <= 0 {
+		return defaultLimitMB * 1024 * 1024
+	}
+	return limitMB * 1024 * 1024
 }
